ae55114e4294de6d988bebbc0383361098673d12
seb! <sebi@sebi.one.pl>
2016-04-10 11:20:24 +0200

    My provisioning scripts: vpn-triggers.ps1, vpn-triggers.reg.

---
+++ provision/powershell/vpn-triggers.ps1
@@ -0,0 +1,25 @@
+﻿# file    : vpn-triggers.ps1
+# author  : seb! <sebi@sebi.one.pl>
+# license : MIT
+
+# This script:
+# * registers VPN application triggers for some executable files:
+#   - Visual Studio IDE,
+#   - eTask,
+# All this thing happens just for more ergonomics.
+
+$current = Get-VpnConnectionTrigger "Soneta VPN"                            `
+              | Select-Object       -ExpandProperty ApplicationID 
+
+Get-ChildItem -Path ${env:ProgramFiles(x86)}, $env:ProgramW6432             `
+                  , $env:LOCALAPPDATA\Apps                                  `
+              -File                                                         `
+              -Recurse                                                      `
+              -Include devenv.exe, eTask.exe                                `
+   | Where-Object { !$current -Or -Not $current.Contains($_.FullName) }     `
+   | ForEach-Object                                                         `
+   {                                                                        `
+       Add-VpnConnectionTriggerApplication -ApplicationID "$($_.FullName)"  `
+                                           -ConnectionName "Soneta VPN"     `
+   }
+ 
