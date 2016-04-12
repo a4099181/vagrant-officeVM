ea3175f5e0e743e7c856b0d91d7a764fb3172b21
seb! <sebi@sebi.one.pl>
2016-04-12 16:32:33 +0200

    Fix #4: My provision script revolutioned: map-drives.ps1.

--- provision/powershell/map-drives.ps1
+++ provision/powershell/map-drives.ps1
@@ -4,19 +4,37 @@
 
 # This script:
 # * takes a drives mappings list to from a JSON formatted text file
+# * stores credentials into Windows Vault
 # * creates a new drive for each entry
+#
+# Some weird resolution made and notation (at least for me) gives
+# a successful credentials storage usable at later time.
+# There is no need to enter username/password to reconnect mapped drives.
 
 $temp = Join-Path $env:LOCALAPPDATA 'Temp'
 $list = Join-Path $temp 'map-drives.json'
+$pass = ConvertTo-SecureString -AsPlainText -Force "vagrant"
+$cred = New-Object pscredential("vagrant",  $pass)
 
-Get-Content                      -Path $list                          `
-   | ConvertFrom-Json                                                 `
-   | Select-Object               -ExpandProperty mappings             `
-   | ForEach-Object                                                   `
-{
-        New-SmbMapping           -LocalPath  $_.local                 `
-                                 -RemotePath $_.remote                `
-                                 -UserName   $_.username              `
-                                 -Password   $_.password              `
-                                 -Persistent $true
+
+Get-Content                   -Path $list                                 `
+   | ConvertFrom-Json                                                     `
+   | Select-Object            -ExpandProperty mappings                    `
+   | ForEach-Object                                                       `
+{                                                                         `
+    Start-Process cmdkey      -Credential $cred                           `
+                              -LoadUserProfile                            `
+                              -NoNewWindow                                `
+                              -Wait `
+           "/add:$($_.server) /user:$($_.username) /pass:$($_.password)"; `
+    Start-Process powershell  -Credential $cred                           `
+                              -LoadUserProfile                            `
+                              -NoNewWindow                                `
+                              -Wait                                       `
+@"
+        New-SmbMapping        -LocalPath       $($_.local)               ``
+                              -RemotePath      $($_.remote)              ``
+                              -Persistent      `$True                    ``
+                              -SaveCredentials
+"@                                                                        `
 }
