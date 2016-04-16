e9fa8b1ec999c7d6e1e144ca22774d6213817735
seb! <sebi@sebi.one.pl>
2016-04-16 22:47:23 +0200

    Fix #7: My provisioning script update: map-drives.ps1.

--- provision/powershell/map-drives.ps1
+++ provision/powershell/map-drives.ps1
@@ -4,12 +4,7 @@
 
 # This script:
 # * takes a drives mappings list to from a JSON formatted text file
-# * stores credentials into Windows Vault
 # * creates a new drive for each entry
-#
-# Some weird resolution made and notation (at least for me) gives
-# a successful credentials storage usable at later time.
-# There is no need to enter username/password to reconnect mapped drives.
 
 $temp = Join-Path $env:LOCALAPPDATA 'Temp'
 $list = Join-Path $temp 'map-drives.json'
@@ -19,14 +14,9 @@ $cred = New-Object pscredential("vagrant",  $pass)
 
 Get-Content                   -Path $list                                 `
    | ConvertFrom-Json                                                     `
-   | Select-Object            -ExpandProperty mappings                    `
+   | Select-Object            -ExpandProperty drives                      `
    | ForEach-Object                                                       `
 {                                                                         `
-    Start-Process cmdkey      -Credential $cred                           `
-                              -LoadUserProfile                            `
-                              -NoNewWindow                                `
-                              -Wait `
-           "/add:$($_.server) /user:$($_.username) /pass:$($_.password)"; `
     Start-Process powershell  -Credential $cred                           `
                               -LoadUserProfile                            `
                               -NoNewWindow                                `
