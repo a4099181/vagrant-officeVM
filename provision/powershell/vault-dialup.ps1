c875b2410248625086cda08024280f624384d9a4
seb! <sebi@sebi.one.pl>
2016-04-17 21:05:28 +0200

    Issue #5: My provisioning script: vault-dialup.ps1.

---
+++ provision/powershell/vault-dialup.ps1
@@ -0,0 +1,19 @@
+# file    : vault-dialup.ps1
+# author  : seb! <sebi@sebi.one.pl>
+# license : MIT
+
+# This script:
+# * takes a dialup credentials list from a JSON formatted text file
+# * stores dialup credentials using dialupass utility.
+
+$temp = Join-Path $env:LOCALAPPDATA 'Temp'
+$list = Join-Path $temp 'vault.json'
+
+Get-Content                   -Path $list                                   `
+   | ConvertFrom-Json                                                       `
+   | Select-Object            -ExpandProperty credentials                   `
+   | Where-Object             { $_.type -eq "dialup" }                      `
+   | ForEach-Object                                                         `
+{                                                                           `
+        dialupass /setpass    `"$($_.name)`" $($_.username) $($_.password); `
+}
