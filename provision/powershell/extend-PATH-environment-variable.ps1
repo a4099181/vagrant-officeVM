e79024031bf90eed363640f47758366c25ace852
Klaudiusz Bryja <klaudiusz.bryja@enova.pl>
2016-10-14 09:27:38 +0200

    Add %UserProfile%\bin to %PATH% environment variable
    This is necessary for Windows Credential Manager

---
+++ provision/powershell/extend-PATH-environment-variable.ps1
@@ -0,0 +1,31 @@
+# file    : extend-PATH-environment-variable.ps1
+# author  : bryjamus <bryjamus@gmail.com>
+# license : MIT
+
+# This script:
+# * Adds to %PTH% environment variable paths
+
+Function AddTo-SystemPath {
+  Param([array] $PathToAdd)
+  $VerifiedPathsToAdd = $Null
+  Foreach ($Path in $PathToAdd) {
+    if ($env:Path -like "*$Path*") {
+      Write-Host "Currnet item in path is: $Path"
+      Write-Host "$Path already exists in Path statement"
+    }
+    else {
+      $VerifiedPathsToAdd += ";$Path"
+      Write-Host "`$VerifiedPathsToAdd updated to contain: $Path"
+    }         
+
+    if($VerifiedPathsToAdd -ne $null) {
+      Write-Host "`$VerifiedPathsToAdd contains: $verifiedPathsToAdd"
+      Write-Host "Adding $Path to Path statement now..."
+      [Environment]::SetEnvironmentVariable("Path",$env:Path + $VerifiedPathsToAdd,"Process")
+    }
+  }
+}
+
+$Path = Join-Path $ENV:UserProfile 'bin'
+
+AddTo-SystemPath(@($Path))
