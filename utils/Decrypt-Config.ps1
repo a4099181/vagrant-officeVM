9c7dd71ee7281fe38ed1f9b50e647bc48e34ad6b
seb! <sebi@sebi.one.pl>
2016-12-21 23:21:03 +0100

    Fix #24: Single configuration file.

---
+++ utils/Decrypt-Config.ps1
@@ -0,0 +1,25 @@
+﻿# file    : Decrypt-Config.ps1
+# author  : seb! <sebi@sebi.one.pl>
+# license : MIT
+
+# This script:
+# * decrypts secret regions of the single configuration file for this project
+# * it overwrites the decrypted version of the config file
+#
+# Usage            : powershell /C   Decrypt-Config.ps1
+#    (inside shell): powershell /C ./Decrypt-Config.ps1
+
+. ".\CryptoLib.ps1"
+
+$cfg = "..\cfg.json"
+$key = "..\.vagrant\my-private.key"
+$jsn = Get-Content $cfg | ConvertFrom-Json
+
+$arr = @( $jsn.cfg.drives, $jsn.cfg.vault )
+
+foreach ($a in $arr)
+{
+    $a | Select-Object -expand secret | ForEach-Object { Decrypt $_ $key }
+}
+
+ConvertTo-Json $jsn -Depth 4 | Out-File -Encoding utf8 $cfg
