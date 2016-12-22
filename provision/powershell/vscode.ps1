6b2f33588029166ae2d97de89a41c6812b1ccf48
seb! <sebi@sebi.one.pl>
2016-12-22 21:34:58 +0100

    Fix #34: vscode-extensions.txt migrated to cfg.json.

--- provision/powershell/vscode.ps1
+++ provision/powershell/vscode.ps1
@@ -6,15 +6,9 @@
 # * takes a Visual Studio Code extensions list to install from a text file
 # * executes VS Code to install extensions
 
-$temp      = Join-Path $env:LOCALAPPDATA 'Temp'
-$list      = Join-Path $temp 'vscode-extensions.txt'
+$json = Get-Content C:\vagrant\cfg.json | ConvertFrom-Json
 
-Get-Content                      -Path $list                                `
-    | Select-String              -Pattern '^#', '^\s*$'                     `
-                                 -NotMatch                                  `
-    | Where-Object               { $_.Line.Split('|')[1].Trim() -eq 'yes' } `
-    | ForEach-Object             {
-
-        $id  =                   $_.Line.Split('|')[0].Trim()
-        code --install-extension $id
-}
+$json.cfg.vscode                                        `
+    | Select-Object  -ExpandProperty extensions         `
+    | Where-Object   { -Not $_.disabled }               `
+    | ForEach-Object { code --install-extension $_.name }
