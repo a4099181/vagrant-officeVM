047900d123d4a51a391121a9363a1243e7e2ea9d
seb! <sebi@sebi.one.pl>
2017-04-09 00:50:55 +0200

    rm -r setup setup.ps1

--- setup/non-privileged.ps1
+++
@@ -1,8 +0,0 @@
-# file    : non-privileged.ps1
-# author  : seb! <sebi@sebi.one.pl>
-# license : MIT
-
-$CfgFile="cfg.json"
-
-Install-VisualStudio2017Extensions $CfgFile
-Install-VisualStudioCodeExtensions $CfgFile
