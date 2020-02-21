e7af782d88ef70377a8dd05907bf3ca602cff93e
seb! <sebi@sebi.one.pl>
2020-02-21 18:35:12 +0100

    Setting up TLS in powershell sessions

--- provision/ruby/powershell.rb
+++ provision/ruby/powershell.rb
@@ -5,13 +5,14 @@
 # My custom solution to provision VM with arbitrary uploaded predefined files.
 $set_working_directory='cd c:\vagrant'
 $powershell_args='-ExecutionPolicy ByPass -NoLogo -NonInteractive -NoProfile -Sta'
+$set_tls='[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072'
 
 def ps_elev vm, inline
     vm.provision "shell", name: inline, powershell_args: $powershell_args,
-        inline: "#{$set_working_directory};#{inline};"
+        inline: "#{$set_working_directory};#{$set_tls};#{inline};"
 end
 
 def ps_nonp vm, inline
     vm.provision "shell", name: inline, powershell_args: $powershell_args, privileged: false,
-        inline: "#{$set_working_directory};#{$import_module};#{inline};"
+        inline: "#{$set_working_directory};#{$import_module};#{$set_tls};#{inline};"
 end
