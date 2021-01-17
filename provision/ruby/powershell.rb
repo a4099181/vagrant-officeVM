c5135664693cdc3c47c034cc407239e808786715
seb! <sebi@sebi.one.pl>
2021-01-17 16:47:56 +0100

    Vagrantfile: powershell-core applied

--- provision/ruby/powershell.rb
+++ provision/ruby/powershell.rb
@@ -2,6 +2,8 @@
 # author  : seb! <sebi@sebi.one.pl>
 # license : MIT
 
+require "base64"
+
 # My custom solution to provision VM with arbitrary uploaded predefined files.
 $set_working_directory='cd c:\vagrant'
 $powershell_args='-ExecutionPolicy ByPass -NoLogo -NonInteractive -NoProfile -Sta'
@@ -15,4 +17,16 @@ end
 def ps_nonp vm, inline
     vm.provision "shell", name: inline, powershell_args: $powershell_args, privileged: false,
         inline: "#{$set_working_directory};#{$import_module};#{$set_tls};#{inline};"
+end
+
+def ps7_elev vm, inline, wd = "c:\\vagrant"
+    enc = Base64.strict_encode64( inline.encode("UTF-16LE") )
+    vm.provision "shell", name: inline, powershell_args: $powershell_args,
+    inline: "pwsh.exe #{$powershell_args} -WorkingDirectory #{wd} -EncodedCommand #{enc}"
+end
+
+def ps7_nonp vm, inline, wd = "c:\\vagrant"
+    enc = Base64.strict_encode64( inline.encode("UTF-16LE") )
+    vm.provision "shell", name: inline, powershell_args: $powershell_args, privileged: false,
+        inline: "pwsh.exe #{$powershell_args} -WorkingDirectory #{wd} -EncodedCommand #{enc}"
 end
